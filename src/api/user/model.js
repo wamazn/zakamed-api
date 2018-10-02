e57383727805d1f6d5a7bcdb87b2b24bac5f24c7
famazan <famazan@tcn.com.ar>
2018-10-02 20:11:06 -0300

    first commit

---
+++ src/api/user/model.js
@@ -0,0 +1,125 @@
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import randtoken from 'rand-token'
+import mongoose, { Schema } from 'mongoose'
+import mongooseKeywords from 'mongoose-keywords'
+import { env } from '../../config'
+
+const roles = ['user', 'admin']
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    match: /^\S+@\S+\.\S+$/,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  firstname: {
+    type: String,
+    index: true,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    index: true,
+    trim: true
+  },
+  services: {
+    google: String
+  },
+  role: {
+    type: String,
+    enum: roles,
+    default: 'user'
+  },
+  picture: {
+    type: String,
+    trim: true
+  }
+}, {
+  timestamps: true
+})
+
+userSchema.path('email').set(function (email) {
+  if (!this.picture || this.picture.indexOf('https://gravatar.com') === 0) {
+    const hash = crypto.createHash('md5').update(email).digest('hex')
+    this.picture = `https://gravatar.com/avatar/${hash}?d=identicon`
+  }
+
+  if (!this.username) {
+    this.username = email.replace(/^(.+)@.+$/, '$1')
+  }
+
+  return email
+})
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+
+  /* istanbul ignore next */
+  const rounds = env === 'test' ? 1 : 9
+
+  bcrypt.hash(this.password, rounds).then((hash) => {
+    this.password = hash
+    next()
+  }).catch(next)
+})
+
+userSchema.methods = {
+  view (full) {
+    let view = {}
+    let fields = ['id', 'firstname', 'lastname','username', 'picture']
+
+    if (full) {
+      fields = [...fields, 'email', 'createdAt']
+    }
+
+    fields.forEach((field) => { view[field] = this[field] })
+
+    return view
+  },
+
+  authenticate (password) {
+    return bcrypt.compare(password, this.password).then((valid) => valid ? this : false)
+  }
+}
+
+userSchema.statics = {
+  roles,
+
+  createFromService ({ service, id, email, name, picture }) {
+      const firstname = name
+    return this.findOne({ $or: [{ [`services.${service}`]: id }, { email }] }).then((user) => {
+      if (user) {
+        user.services[service] = id
+        user.firstname = firstname
+        user.picture = picture
+        return user.save()
+      } else {
+        const password = randtoken.generate(16)
+        return this.create({ services: { [service]: id }, email, password, firstname, picture })
+      }
+    })
+  }
+}
+
+userSchema.plugin(mongooseKeywords, { paths: ['email', 'name'] })
+
+const model = mongoose.model('User', userSchema)
+
+export const schema = model.schema
+export default model
